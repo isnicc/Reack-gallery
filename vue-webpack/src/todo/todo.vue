2b098e7d39b6b550a7e58fdb72c8ce3ceab98358
isni <isniccweb@gmail.com>
2018-08-20 00:11:00 +0800

    keyup回车应用

--- vue-webpack/src/todo/todo.vue
+++ vue-webpack/src/todo/todo.vue
@@ -1,3 +1,24 @@
 <template>
-  <div>asdf</div>
+  <div class="xain">
+    <input v-model="content" @keyup.enter="todofun" placeholder="请输入内容">
+    <div>
+      {{content}}
+    </div>
+  </div>
 </template>
+<script>
+import '../assets/styles/todo.styl'
+//注意事项：styl 样式不支持在组件内写，需要以文件形式引入
+export default {
+  data(){
+    return {
+      content:'内容展示'
+    }
+  },
+  methods:{
+    todofun(){
+      console.log(this.content)
+    }
+  }
+}
+</script>
