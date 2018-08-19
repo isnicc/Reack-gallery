40a713d0622ad6ea949b036be0016523052d89dd
isni <isniccweb@gmail.com>
2018-08-19 22:48:05 +0800

    webpack + vue开发环境构建

---
+++ vue-webpack/src/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app">
+  {{test}}
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      test:'this is web test!'
+    }
+  },
+  created(){
+    console.log('3333')
+  }
+}
+</script>
+<style>
+.app{
+  color:red;
+}
+</style>
---
+++ vue-webpack/webpack.config.js
@@ -0,0 +1,78 @@
+const path=require('path')
+const webpack=require('webpack')
+const HTMLPlugin=require('html-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
+
+const isDev=process.env.NODE_ENV === 'development'
+
+const config={
+  target:'web',
+  entry:path.join(__dirname,'src/index.js'),
+  output:{
+    filename:'bundle.js',
+    path:path.join(__dirname,'dist')
+  },
+  module:{
+    rules:[
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test:/\.css$/,
+        use:[
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test:/\.styl$/,
+        use:[
+          'style-loader',
+          'css-loader',
+          'stylus-loader'
+        ]
+      },
+      {
+        test:/\.(jpg|jpeg|gif|png|svg)$/,
+        use:[
+          {
+            loader:'url-loader',
+            options:{
+              limit:1024,
+              name:'[name]-qqq.[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        NODE_ENV:isDev?'"development"':'"production"'
+      }
+    }),
+    new HTMLPlugin(),
+    new VueLoaderPlugin()
+  ]
+}
+
+if(isDev){
+  config.devtool ='eval'//'source-map'
+  config.devServer={
+    port:8000,
+    host: 'localhost',//0.0.0.0 不可以直接访问，但支持用localhost,127.0.0.0等方式打开
+    overlay:{
+      errors:true
+    },
+    hot:true,//热替换
+    // https:true,//启用https
+    open:true
+  }
+  config.mode='development'
+}else{
+  config.mode='production'
+}
+
+module.exports=config
